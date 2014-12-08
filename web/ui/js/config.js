0d496c71c2b237817d72a18f371786e6b2ca0ef9
Tony Zhou <zong-bo.zhou@hp.com>
2014-12-08 16:30:48 +0800

    tony commit

--- web/ui/js/config.js
+++ web/ui/js/config.js
@@ -3,7 +3,7 @@
  */
 TM.constant('TM.config',
     {
-            RESTAPIBASEURL: 'http://localhost:8080/api/'
+            RESTAPIBASEURL: 'http://localhost:8080/manner/'
 //            RESTAPIBASEURL: 'http://16.187.38.170:8002/manner-ci/api/'
     }
 );
