12158bf98b3d6c7a772ffb2a2ffcc6d2fcde5825
ncistyang <ncistyang3@gmail.com>
2015-01-05 12:34:17 +0800

    Link

--- web/ui/js/controllers/personalActionController.js
+++ web/ui/js/controllers/personalActionController.js
@@ -28,7 +28,7 @@ controller('personalActionController', ['$scope','$filter','ngTableParams','$htt
     	if($scope.data.length>0){
     	    $scope.tableParams = new ngTableParams({
     	        page: 1,            // show first page
-    	        count: 10          // count per page
+    	        count: 20          // count per page
     	    }, {
     	    	counts: [], // hide page counts control
     	        groupBy: 'itemType',
@@ -47,98 +47,30 @@ controller('personalActionController', ['$scope','$filter','ngTableParams','$htt
     });
     $scope.tableParams = new ngTableParams();
     
-    $scope.showDetails = function(user) {
-        console.info(user);
-//        var modalInstance = $modal.open({
-//            templateUrl: 'myModalContent.html',
-//            controller: 'ModalInstanceCtrl',
-//            size: size,
-//            resolve: {
-//              items: function () {
-//                return $scope.items;
-//              }
-//            }
-//          });
-//
-//          modalInstance.result.then(function (selectedItem) {
-//            $scope.selected = selectedItem;
-//          }, function () {
-//            $log.info('Modal dismissed at: ' + new Date());
-//          });
-        };
+    $scope.detailObj = {};
+    
+    $scope.showDetails = function(detaiObj) {
+        console.info(detaiObj);
+	    $http.get(detaiObj._links.group.href).success(function (group) {
+	    	detaiObj.group = group;
+	    });
+	    $http.get(detaiObj._links.owner.href).success(function (owner) {
+	    	detaiObj.owner = owner;
+	    });
+	    $http.get(detaiObj._links.lastModifiedBy.href).success(function (lastModifiedBy) {
+	    	detaiObj.lastModifiedBy = lastModifiedBy;
+	    });
+	    $http.get(detaiObj._links.postBy.href).success(function (postBy) {
+	    	detaiObj.postBy = postBy;
+	    });
+	    $http.get(detaiObj._links.item.href).success(function (item) {
+	    	detaiObj.item = item;
+	    });
+	    $http.get(detaiObj._links.createdBy.href).success(function (createdBy) {
+	    	detaiObj.createdBy = createdBy;
+	    });
+        $scope.detailObj = detaiObj;
+    };
+    
+    
 }]);
-
-
-
-
-
-
-
-
-
-
-
-var app = angular.module('DemoMock', ['ngTable', 'ngMockE2E'])
-.run(function($httpBackend, $filter, $log, ngTableParams) {
-    // emulation of api server
-    $httpBackend.whenGET(/data.*/).respond(function(method, url, data, headers) {
-        var query = url.split('?')[1],
-            requestParams = {};
-
-        $log.log('Ajax request: ', url);
-
-        var vars = query.split('&');
-        for (var i = 0; i < vars.length; i++) {
-            var pair = vars[i].split('=');
-            requestParams[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
-        }
-        // parse url params
-        for (var key in requestParams) {
-            if (key.indexOf('[') >= 0) {
-                var params = key.split(/\[(.*)\]/), value = requestParams[key], lastKey = '';
-
-                angular.forEach(params.reverse(), function(name) {
-                    if (name != '') {
-                        var v = value;
-                        value = {};
-                        value[lastKey = name] = isNumber(v) ? parseFloat(v) : v;
-                    }
-                });
-                requestParams[lastKey] = angular.extend(requestParams[lastKey] || {}, value[lastKey]);
-            } else {
-                requestParams[key] = isNumber(requestParams[key]) ? parseFloat(requestParams[key]) : requestParams[key];
-            }
-        }
-
-        data = [{name: "Moroni", age: 50},
-                {name: "Tiancum", age: 43},
-                {name: "Jacob", age: 27},
-                {name: "Nephi", age: 29},
-                {name: "Enos", age: 34},
-                {name: "Tiancum", age: 43},
-                {name: "Jacob", age: 27},
-                {name: "Nephi", age: 29},
-                {name: "Enos", age: 34},
-                {name: "Tiancum", age: 43},
-                {name: "Jacob", age: 27},
-                {name: "Nephi", age: 29},
-                {name: "Enos", age: 34},
-                {name: "Tiancum", age: 43},
-                {name: "Jacob", age: 27},
-                {name: "Nephi", age: 29},
-                {name: "Enos", age: 34}];
-
-        var params = new ngTableParams(requestParams);
-        data = params.filter() ? $filter('filter')(data, params.filter()) : data;
-        data = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
-
-        var total = data.length;
-        data = data.slice((params.page() - 1) * params.count(), params.page() * params.count());
-
-        return [200, {
-            result: data,
-            total: total
-        }];
-    });
-    $httpBackend.whenGET(/.*/).passThrough();
-});
